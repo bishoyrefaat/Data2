13a015c258d21caaf174eaab1fc145d8f3a11699
bishoyrefaat <48106126+bishoyrefaat@users.noreply.github.com>
2020-06-01 03:42:13 +0200

    Revert "more minor stuff -__-"
    This reverts commit 5b00ef3df47a7f7330a0d578a4c7f14103b6ec70.

--- Combined-java-version/Data2Final/src/part3.java
+++ Combined-java-version/Data2Final/src/part3.java
@@ -1,25 +1,25 @@
-import javax.print.attribute.IntegerSyntax;
 import java.util.*;
 
 public class part3 extends AbstractPart{
 
     static class Route {
-         int point, weight,time ;
-        public Route(int point, int weight,int time) {
+         int point, weight;
+        public Route(int point, int weight) {
             this.point = point;
             this.weight = weight;
-            this.time = time;
         }}
 
     class RouteComparator implements Comparator<Route>{
         public int compare(Route s1, Route s2) {
-            if (s1.weight<s2.weight)return 1;
-            else if (s1.weight>s2.weight)return -1;
-            return 0; }
+            if (s1.weight < s2.weight) return 1;
+            else if (s1.weight > s2.weight) return -1;
+            return 0;
+        }
     }
 
     int M;
     ArrayList<ArrayList<Route>> graph;
+    int[][] g;
     public void start() {
         Scanner s = new Scanner(System.in);
         System.out.println("please enter amount M:");
@@ -31,12 +31,14 @@ public class part3 extends AbstractPart{
         nr=s.nextInt();
         System.out.println("please enter source , destination time and cost for each route: ");
         graph=new ArrayList<ArrayList<Route>> (nr+1);
+        g=new int[nc+1][nc+1];
         for (int i = 0; i < nc+1; i++){graph.add(new ArrayList<Route>(nc+1));}
         int src , dest ,time,cst,total;
         for (int i = 0; i < nr; i++){
             src=s.nextInt();dest=s.nextInt();time=s.nextInt();cst=s.nextInt();
             total=M*(1+time)+cst; //get total cost by adding the hour cost (with the 1 hour stay included)
-            graph.get(src).add(new Route(dest,total,time));
+            graph.get(src).add(new Route(dest,total));
+            g[src][dest]=time;
         }
         System.out.println("please enter source city:");
         int start=s.nextInt();
@@ -48,27 +50,26 @@ public class part3 extends AbstractPart{
     }
 
     private void dijkstra(int start,int end,int nc,int nr) {
-            int d[] = new int[nc+1];//array to contain the shortest distance for each point relative to the start
-            int t[]= new int[nc+1];//array to record the time for the shortest path for each point relative to the start
+            int d[] = new int[nc+1];// this array is to contain the shortest distance for each point from to the start
             int shortest[]= new int[nc+1];
             /* this will contain the previous point for each shortest path.
             for example in the case provided with the assigment  d[4]=3 , and d[3]=1
             this is used to trace the path taken  */
             for (int i = 0; i < nc+1; i++) {d[i] = -1; }//initial values for all points
-            d[start]=0;//start is intialized with 0 distance and time
+            d[start]=0;//start is intialized with 0
             PriorityQueue<Route> q = new PriorityQueue<Route>(nr+1,new RouteComparator());
-            q.add(new Route(start,0,0));
+            q.add(new Route(start,0));
             while(!q.isEmpty()&&end!=start){
                 int cur=q.poll().point;
                 Iterator<Route> iter = graph.get(cur).iterator(); //iterate from each point to its available routes
                 while(iter.hasNext()){
                     Route n=iter.next();
-                    int nxt=n.point,weight=n.weight,time=n.time;
+                    int nxt=n.point;
+                    int weight=n.weight;
                     //check if a route through the current point creates a shorter path, and update d[]
                     if(d[nxt]>d[cur]+weight||d[nxt]==-1){
                         d[nxt]=d[cur]+weight;
-                        t[nxt]=time+t[cur]+1;//added 1 to time to account for the 1 hour wait in each city
-                        q.add(new Route(nxt,d[nxt],t[nxt]));//enqueue the point after the update
+                        q.add(new Route(nxt,d[nxt]));//enqueue the point after the update
                         shortest[nxt]=cur;
                     }
                 }
@@ -76,22 +77,22 @@ public class part3 extends AbstractPart{
             if(d[end]!=-1) {
 
                 System.out.print("The route with minimum cost is ");
-                printPathRec(shortest, end, start);
+                int hours = printPathRec(shortest, end, start);
                 System.out.println();
-                System.out.print("Total time " + Math.max(0,(t[end]-1))+ " hours \nTotal cost = " + Math.max(0, (d[end] - 1 * M)) + "$");
+                System.out.print("Total time " + (hours - 1) + " hours \nTotal cost = " + Math.max(0, (d[end] - 1 * M)) + "$");
                 // minus 1*M because no stoping for 1 hour in last point
             }else
                 System.out.println("no path available");
     }
 
 
-    public void printPathRec(int [] d,int b,int a) {
-        if(b==a){ System.out.print(b);
-        } else {
-            printPathRec(d, d[b], a);
-            System.out.print(" "+b); }
+    public int printPathRec(int [] d,int end,int start) {
+        if(end==start){ System.out.print(start);return 0;}
+        else {
+             int i = printPathRec(d, d[end], start);
+            System.out.print(" "+end);
+        return i+g[d[end]][(end)]+1;}
     }
-
 
     }
 //test ex   100  4  5    1 2 1 250   1 3 1 300   1 4 2 700   2 4 1 300   3 4 1 200   1  4
